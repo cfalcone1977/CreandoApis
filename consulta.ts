ce2ac68007b232e07f665117f249902446c3d29c
Cristian Falcone <cristianfalcone@hotmail.com>
2025-06-09 18:21:08 -0300

    Crear API con express

--- consulta.ts
+++
@@ -1,9 +0,0 @@
-import readlineSync from "readline-sync";
-
-
-const moneda:string="ARS";
-const cantidad:number=100000000;
-let pesos:number;
-
-pesos=readlineSync.questionInt("Ingrese la cantidad de pesos:");
-
